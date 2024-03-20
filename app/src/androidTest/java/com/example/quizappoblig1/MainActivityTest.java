94a95ca9dfe38829ca79877cf066a2776a6b1d5f
Sondre <89258011+h600853@users.noreply.github.com>
2024-03-20 11:42:12 +0100

    main activity tests

--- app/src/androidTest/java/com/example/quizappoblig1/MainActivityTest.java
+++ app/src/androidTest/java/com/example/quizappoblig1/MainActivityTest.java
@@ -3,10 +3,12 @@ package com.example.quizappoblig1;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -21,16 +23,24 @@ public class MainActivityTest {
             = new ActivityScenarioRule<>(MainActivity.class);
     @Test
     public void testGalleryButton() {
+        Intents.init();
+
      onView(withId(R.id.gallery)).check(matches(isDisplayed()));
      onView(withId(R.id.gallery)).check(matches(isClickable()));
      onView(withId(R.id.gallery)).perform(click());
-     onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
+
+     Intents.intended(hasComponent(GalleryActivity.class.getName()));
+     Intents.release();
     }
     @Test
     public void testQuizButton() {
+        Intents.init();
+
         onView(withId(R.id.quiz)).check(matches(isDisplayed()));
         onView(withId(R.id.quiz)).check(matches(isClickable()));
         onView(withId(R.id.quiz)).perform(click());
-        onView(withId(R.id.imageView)).check(matches(isDisplayed()));
+
+        Intents.intended(hasComponent(QuizActivity.class.getName()));
+        Intents.release();
     }
 }
